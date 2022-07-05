3c010cd691ba1e497b9c08adbb6dd093fb21c8a1
crishood <crishood13@gmail.com>
2022-07-05 14:19:46 -0500

    Configure mongoose with mongo Atlas uri

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { connect } = require("./src/db");
+require("dotenv").config({ path: "./.env" });
 
 const PORT = 8080;
 
 const app = express();
 app.use(cors());
+connect();
 app.use(express.json());
 app.use(morgan("dev"));
 
